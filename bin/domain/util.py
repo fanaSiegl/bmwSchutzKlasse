805826650b0374490ab338554a97f2ac1b581b59
Frantisek Siegl <frantisek.siegl@idiada.cz>
2020-03-03 11:39:01 +0100

    First test version.

--- bin/domain/util.py
+++ bin/domain/util.py
@@ -53,4 +53,55 @@ def runSubprocess(command, cwd=None):
     return stdout, stderr
 
 #==============================================================================
-    
+
+DEFINITION_STRING_SURFACE = '''**
+** SURFACE DEFINITIONS 
+**
+'''
+DEFINITION_STRING_SET = '''** 
+** SETS
+**
+'''
+DEFINITION_STRING_STEP = '''**
+** Step
+**
+'''
+DEFINITION_STRING_STEP_TYPE = '''*STEP, NAME = Step_Schutzklassensets
+*DYNAMIC, EXPLICIT, ELEMENT BY ELEMENT
+,             0.15
+'''
+DEFINITION_STRING_STEP_OUTPUT_FILED = '*OUTPUT, FIELD, TIME INTERVAL=0.004\n'
+DEFINITION_STRING_STEP_OUTPUT_HISTORY = '*OUTPUT, HISTORY, TIME INTERVAL=0.0001, FILTER=ANTIALIASING\n'
+DEFINITION_STRING_PARAMETERS = '''*PARAMETER
+QI_INCLPRE = 0
+QI_SQMS = 1.02
+QI_NK = 1548406903
+schutzklasse1 = 0
+schutzklasse2 = 5
+schutzklasse3 = 20
+schutzklasse4 = 25
+schutzklasse5 = -1000
+schutzklasse6 = 1000
+**
+** Lastfallabhaengiger Zuendzeitpunkt
+**
+** Pfahl Front
+**zzpSchutzklasse = 34
+** USNCAP
+**zzpSchutzklasse = 10
+** FMVSS301
+zzpSchutzklasse = 25
+** SOL
+**zzpSchutzklasse = 25
+** FOO
+**zzpSchutzklasse = 15
+**
+** Set Postfix
+**
+schutzPostfix1 = schutzklasse1 + zzpSchutzklasse
+schutzPostfix2 = schutzklasse2 + zzpSchutzklasse
+schutzPostfix3 = schutzklasse3 + zzpSchutzklasse
+schutzPostfix4 = schutzklasse4 + zzpSchutzklasse
+schutzPostfix5 = schutzklasse5 + zzpSchutzklasse
+schutzPostfix6 = schutzklasse6 + zzpSchutzklasse
+'''
--- bin/main.py
+++ bin/main.py
@@ -2,33 +2,30 @@
 # -*- coding: utf-8 -*-
 
 '''
-Project name
-============
+bmwSchutzKlasse
+===============
 
-Project function description.
-
-* does requires something
-* does something
-* creates something as an output
+Creates sets from given *.xlsx configuration file and after adding entities to these sets, they can be exported into an include file.
 
 Location
 --------
 
-buttonGroupName>buttonName
+BMW>schutzKlasse
 
 Usage
 -----
 
-* click here
-* click there
-* click there
+* select configuration *.xlsx file
+* create sets
+* assign entities to sets
+* export include
 
 Requirements
 ------------
 
 .. warning::
     
-    Requires something
+    Given configuration file must be in the standard format.
     
 * designed for ANSA V.19.1.1
 
@@ -36,23 +33,24 @@ Requirements
 
 #=========================== to be modified ===================================
 
-BUTTON_NAME = 'buttonName'
-BUTTON_GROUP_NAME = 'buttonGroupName'
+BUTTON_NAME = 'BMW'
+BUTTON_GROUP_NAME = 'schutzKlasse'
 DOCUMENTATON_GROUP = 'ANSA tools'
-DOCUMENTATON_DESCRIPTION = 'Python application one line description.'
+DOCUMENTATON_DESCRIPTION = 'ANSA button for Schutz Klasse sets/include definition.'
 
 #==============================================================================
 
-DEBUG = 1
+DEBUG = 0
 
 #==============================================================================
 
 import os
 import sys
 from traceback import format_exc
+import tempfile
 
 import ansa
-from ansa import base, guitk, constants
+from ansa import base, guitk, constants, utils
 
 PATH_SELF = os.path.dirname(os.path.realpath(__file__))
 
@@ -60,20 +58,354 @@ ansa.ImportCode(os.path.join(PATH_SELF, 'domain', 'util.py'))
 
 #==============================================================================
 
-class Project(object):
+class SetDefinition(object):
+		
+	ID_OFFSET = 100
+	
+	def __init__(self, parentApplication, attributes):
+		
+		self.parentApplication = parentApplication
+		self._attributes = attributes
+		self.id = self._attributes['id']
+		self.setEntity = None
+		self.content = list()
+		
+		self.tableColumnLabels = parentApplication.tableColumnLabels
+	
+	#--------------------------------------------------------------------------
+	
+	def getName(self, includeId=True):
+		
+		nameParts = []
+		for attrName in self.tableColumnLabels[1:]:
+			attrValue = self._attributes[attrName]
+			if attrValue is None or len(attrValue) == 0:
+				continue
+			
+			nameParts.append(str(attrValue))
+		
+		if includeId:
+			return '%i;' % self.id + '_'.join(nameParts)
+		else:
+			return '_'.join(nameParts)
+			
+	#--------------------------------------------------------------------------
+	
+	def getSurfaceDefinition(self):
+		
+		''' *SURFACE, NAME=G"+set+", TYPE=ELEMENT'''
+		
+		return '*SURFACE, NAME=G%s, TYPE=ELEMENT\nS%s,\n' % (self.getName(), self.getName())
+	
+	#--------------------------------------------------------------------------
+	
+	def getSetDefinition(self):
+						
+		return '*ELSET, ELSET=S%s\n' % self.getName() + ''.join(self.content)
+		
+	#--------------------------------------------------------------------------
+	
+	def getOutputForceDefinition(self):
+		
+		return '*CONTACT OUTPUT, SURFACE=G%s\nCFORCE,\n' % self.getName()
+	
+	#--------------------------------------------------------------------------
+	
+	def getOutputEnergyDefinition(self):
+		
+		return '''*CONTACT OUTPUT, SURFACE=G%s
+CFN
+CFNM,
+*ENERGY OUTPUT, ELSET=S%s
+"ALLIE,
+''' % (self.getName(), self.getName())
+	
+	#--------------------------------------------------------------------------
+	
+	def createEntity(self):
+		
+		fields = {
+			'Name' : self.getName(False),
+			'Output as:' : 'Set',
+			'OUTPUT TYPE' : 'ELSET'
+			}
+		self.setEntity = base.CreateEntity(constants.ABAQUS, 'SET', fields, include=self.parentApplication.include)
+		
+		if self.setEntity is not None:
+			base.SetEntityId(self.setEntity, self.id)
+	
+	#--------------------------------------------------------------------------
+	
+	def identifyEntity(self):
+		
+		self.setEntity = base.GetEntity(constants.ABAQUS, 'SET', self.id)
+		
+	#--------------------------------------------------------------------------
+	
+	def addContentLine(self, line):
+		
+		self.content.append(line)
+		
 
-	APPLICATION_NAME = 'New project name'
+#==============================================================================
 
+class SchutzKlasseSetsCreator(object):
+	
+	DEFINITION_FILE_NAME = 'E_E_Komponenten_G60PHEV_AWD_VL-VKBG.xlsx'
+	LABELS_ROW_NO = 0
+	
 	def __init__(self):
+		
+		self.setItems = list()
+		self.inputConfigFile = None
+		
+	#--------------------------------------------------------------------------
+	
+	def setConfigFilePath(self, inputConfigFile):
+		
+		self.inputConfigFile = inputConfigFile
+			
+	#--------------------------------------------------------------------------
+				
+	def createSets(self):
+		
+		self.createInclude()
+		self.readDefinition()
+		
+		info = list()
+		for setItem in self.setItems:
+			 setItem.createEntity()
+			 info.append(setItem.getName(False))
+		
+		showInfoMessage('Sets created:\n'+'\n'.join(info))
+	
+	#--------------------------------------------------------------------------
+	
+	def identifySets(self):
+		
+		# identify entities
+		setEntities = list()
+		setDefinitions = dict()
+		for setItem in self.setItems:
+			 setItem.identifyEntity()
+			 
+			 if setItem.setEntity is not None:
+				 setEntities.append(setItem.setEntity)
+				 setDefinitions['*ELSET, ELSET=%s' % setItem.getName(False).strip()] = setItem
+			 
+		# identify include
+		self.createInclude()
+		
+		base.AddToInclude(self.include, setEntities)
+		
+		tempName = tempfile.NamedTemporaryFile(suffix='.inc').name
+		
+		# output include
+		ret = base.OutputAbaqus(
+			filename=tempName,
+			write_comments='none',
+			include=self.include)
+		
+		if ret:
+			fi = open(tempName, 'rt')
+			lines = fi.readlines()
+			
+			currentSetItem = None
+			for line in lines:
+				if line.startswith('*'):
+					if line.strip() in setDefinitions.keys():
+						currentSetItem = setDefinitions[line.strip()]
+						continue
+					else:
+						currentSetItem = None
+					
+				if currentSetItem is not None:
+					currentSetItem.addContentLine(line)
+				
+			fi.close()
+		else:
+			showCriticalMessage('Something went wrong with the include output.\nPlease output include with sets manually.')
+		
+		if os.path.exists(tempName):
+			os.remove(tempName)
+			
+	#--------------------------------------------------------------------------
+	
+	def createInclude(self):
 
-		self.method1()
-
+		fields = {'Name' : 'sets_for_schutzklasse'}
+		self.include = base.CreateEntity(constants.ABAQUS, 'INCLUDE', fields)
+		
 	#--------------------------------------------------------------------------
 
-	def method1(self):
+	def readDefinition(self):
 		
-		showInfoMessage('%s\n%s' % (self.APPLICATION_NAME, util.getVersionInfo()))
+#		if DEBUG:
+#			self.inputConfigFile = os.path.join(util.PATH_RES, self.DEFINITION_FILE_NAME)
+			
+		xl_element = utils.XlsxOpen(self.inputConfigFile)
+		sheetName = utils.XlsxGetSheetName(xl_element, 0)
+		
+		rowCount, colCount = utils.XlsxMaxSheetCell(xl_element, sheetName)
+		
+		self.definition = dict()
+		self.tableColumnLabels = list()
+		for rowNo in range(rowCount):
+			descriptionNo = utils.XlsxGetCellValue(xl_element, sheetName, rowNo , 0)
+			
+			# skip empty rows
+			if descriptionNo is None or len(descriptionNo) == 0:
+				continue
+			
+			# identify table labels
+			if rowNo == self.LABELS_ROW_NO:
+				for cNo in range(colCount):
+					cellValue = utils.XlsxGetCellValue(xl_element, sheetName, rowNo , cNo)
+					if cellValue is None or len(cellValue) == 0:
+						break
+					self.tableColumnLabels.append(cellValue)
+				continue
+			
+			self.definition[descriptionNo] = dict()
+			for columnNo, label in enumerate(self.tableColumnLabels):
+				
+				cellValue = utils.XlsxGetCellValue(xl_element, sheetName, rowNo , columnNo)			
+				
+				# set id
+				if columnNo == 0:
+					self.definition[descriptionNo]['id'] = int(cellValue) + SetDefinition.ID_OFFSET
+				
+				# rename Schutzklasse
+				if label.strip().lower() == 'schutzklasse':
+					temp = int(''.join(filter(str.isdigit, cellValue)))
+					cellValue = cellValue + "_<schutzPostfix"+str(temp)+">"
+				
+				self.definition[descriptionNo][label] = cellValue.strip().replace(' ', '_')
+				
+			setItem = SetDefinition(self, self.definition[descriptionNo])
+			self.setItems.append(setItem)
+		
+		utils.XlsxClose(xl_element)
+		
+	#--------------------------------------------------------------------------
 
+	def writeInclude(self, fileName):
+		
+		self.readDefinition()
+		self.identifySets()
+					
+#		fileName = os.path.join(util.PATH_RES, 'schutz.inc')
+		f = open(fileName,"w+")
+		
+		f.write(util.DEFINITION_STRING_PARAMETERS)
+		f.write(util.DEFINITION_STRING_SURFACE)
+		
+		for setItem in self.setItems:
+			 f.write(setItem.getSurfaceDefinition())
+			 		
+		f.write(util.DEFINITION_STRING_SET)
+		
+		for setItem in self.setItems:
+			 f.write(setItem.getSetDefinition())
+				
+		f.write(util.DEFINITION_STRING_STEP)
+		f.write(util.DEFINITION_STRING_STEP_TYPE)
+		f.write(util.DEFINITION_STRING_STEP_OUTPUT_FILED)
+				
+		for setItem in self.setItems:
+			 f.write(setItem.getOutputForceDefinition())
+		
+		f.write(util.DEFINITION_STRING_STEP_OUTPUT_HISTORY)
+		
+		for setItem in self.setItems:
+			 f.write(setItem.getOutputEnergyDefinition())
+		
+		f.write("*END STEP")
+		f.close()  
+
+# ==============================================================================
+
+class SchutzKlasseDialog(object):
+	
+	TITLE = 'Schutz Klasse'
+	WIDTH = 600
+	HEIGTH = 150
+	
+	def __init__(self):
+		
+		revision, modifiedBy, lastModified = util.getVersionInfo()
+		
+		self.dialog = guitk.BCWindowCreate("%s (%s)" % (self.TITLE, revision), guitk.constants.BCOnExitDestroy)
+		
+		self.creator = SchutzKlasseSetsCreator()
+		
+		guitk.BCWindowSetInitSize(self.dialog, self.WIDTH, self.HEIGTH)
+		guitk.BCWindowSetSaveSettings(self.dialog, False)
+		
+		self.mainLayout = guitk.BCBoxLayoutCreate(self.dialog, guitk.constants.BCVertical)
+		
+		# input path
+		configFileLayout = guitk.BCBoxLayoutCreate(self.dialog, guitk.constants.BCHorizontal)
+		guitk.BCLabelCreate(configFileLayout, 'Input configuration file')
+		
+		self.inputConfigFileLineEdit = guitk.BCLineEditPathCreate(configFileLayout, guitk.constants.BCHistoryFiles,
+			os.path.dirname(base.DataBaseName()), guitk.constants.BCHistorySelect, 'schutzKlasseInputConfigFile')
+		guitk.BCLineEditPathSetFilter(self.inputConfigFileLineEdit, 'MS Excel files (*.xlsx)')
+		
+#		# output path
+#		configFileLayout = guitk.BCBoxLayoutCreate(self.dialog, guitk.constants.BCHorizontal)
+#		guitk.BCLabelCreate(configFileLayout, 'Output include file')
+#		self.outputIncludeFileLineEdit = guitk.BCLineEditPathCreate(configFileLayout, guitk.constants.BCHistoryFiles,
+#			os.path.dirname(base.DataBaseName()), guitk.constants.BCHistorySaveAs, 'schutzKlasseOutInclude')
+#		guitk.BCLineEditPathSetFilter(self.outputIncludeFileLineEdit, 'ABAQUS (*.inc)')
+		
+		dbb = guitk.BCDialogButtonBoxCreate(self.dialog)
+		acceptButton = guitk.BCDialogButtonBoxGetAcceptButton(dbb)
+		guitk.BCButtonSetText(acceptButton, 'Create sets')
+		b = guitk.BCPushButtonCreate(dbb, 'Export include', self.exportInclude, None)
+		guitk.BCDialogButtonBoxAddButton(dbb, b)
+		
+		guitk.BCWindowSetAcceptFunction(self.dialog, self.createSets, None)
+		
+		guitk.BCShow(self.dialog)
+	
+	#--------------------------------------------------------------------------
+
+	def exportInclude(self, b, data=None):
+		
+		path = guitk.BCLineEditPathLineEditText(self.inputConfigFileLineEdit)
+		if len(path) == 0 or not os.path.exists(path):
+			
+			showCriticalMessage('Given configuration file does not exist!')
+			return
+		else:
+			self.creator.setConfigFilePath(path)
+		
+		path = utils.SelectSaveFileIn(os.path.dirname(base.DataBaseName()), 'ABAQUS (*.inc)')
+		if len(path) == 0:
+			return
+			
+		self.creator.writeInclude(path[0])
+		
+		showInfoMessage('Saved to: "%s"' % path[0])
+		
+		guitk.BCDestroyLater(self.dialog)
+	
+	#--------------------------------------------------------------------------
+
+	def createSets(self, w, data=None):
+		
+		path = guitk.BCLineEditPathLineEditText(self.inputConfigFileLineEdit)
+		if len(path) == 0 or not os.path.exists(path):
+			
+			showCriticalMessage('Given configuration file does not exist!')
+			return 0
+		
+		self.creator.setConfigFilePath(path)
+		self.creator.createSets()
+		
+		return 1
+		
 
 # ==============================================================================
 
@@ -98,7 +430,7 @@ def main():
 	main.__doc__ = __doc__
 
 	try:
-		project = Project()
+		project = SchutzKlasseDialog()
 	except Exception as e:
 		print(format_exc())
 		showCriticalMessage(str(e))
